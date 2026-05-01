24e0a7e32905a4879d6bb2a526032d62b17a81e9
chamelewan <wani001110@gmail.com>
2026-05-01 11:04:57 +0900

    feat: ApiResponse 응답 통일 객체 추가

---
+++ src/main/java/com/example/umc10th_kaito/global/apiPayload/ApiResponse.java
@@ -0,0 +1,23 @@
+package com.example.umc10th_kaito.global.apiPayload;
+import com.example.umc10th_kaito.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th_kaito.global.apiPayload.code.BaseSuccessCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    private T result;
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode successCode, T result) {
+        return new ApiResponse<>(true, successCode.getCode(), successCode.getMessage(), result);
+    }
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode errorCode, T result) {
+        return new ApiResponse<>(false, errorCode.getCode(), errorCode.getMessage(), result);
+    }
+}
